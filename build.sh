41b917c4cb4019b3ca7b32c6fd092dcdd682bc91
Joshua Copeland <joshuarcopeland@gmail.com>
2018-11-20 15:14:10 -0800

    added build directory and ignores

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 composer.phar install --no-ansi --no-dev --no-interaction --no-progress --no-scripts --optimize-autoloader
 rm config.php config.php.dist README.md phpcs.xml composer.* build.sh
-7z a -tzip ../parler-wordpress-php.zip ../parler-wordpress-php
+7z a -tzip ./build/parler-wordpress-php-$(date +%Y-%m-%d).zip ../parler-wordpress-php
