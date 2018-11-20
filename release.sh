da45cc859e3da313303d95ac6807a8c14fa8389e
Joshua Copeland <joshuarcopeland@gmail.com>
2018-11-20 15:35:06 -0800

    new code for release script

--- release.sh
+++ release.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 /usr/local/bin/php composer.phar install --no-ansi --no-dev --no-interaction --no-progress --no-scripts --optimize-autoloader
 rm config.php config.php.dist README.md phpcs.xml composer.* > /dev/null 2>&1
-zip -r ./../parler-wordpress-php-$(date +%Y-%m-%d).zip ./parler-for-wordpress -x "*.DS_Store"
+zip -r ./build/parler-wordpress-php-$(date +%Y-%m-%d).zip ./parler-for-wordpress -x "*.DS_Store"
 echo "Release zipped in /build directory, please run \"composer.phar install && git checkout -- .\" to get back dev files"
