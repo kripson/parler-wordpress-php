70e731843c10ad9483a426ab7c093d70f86a6441
Joshua Copeland <joshuarcopeland@gmail.com>
2018-12-05 12:09:59 -0800

    Updated release script

--- release.sh
+++ release.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash
+
+# Create build directory
+BuildDir="../build/"
+mkdir ${BuildDir}
+rsync -av --progress ./ ${BuildDir} --exclude .git/ --exclude .idea/
+cd ${BuildDir}
+
+rm -r vendor/ > /dev/null 2>&1
+
 # Install composer deps
-/usr/local/bin/php composer.phar install --no-ansi --no-dev --no-interaction --no-progress --no-scripts --optimize-autoloader
-CurDate=$(date +%Y%m%d)
-BuildDir="./build/$CurDate"
-mkdir $BuildDir
-cp -r ./ $BuildDir
-cd $BuildDir
-rm -r ./build ./.git/ ./.gitignore ./.idea/ ./config.php ./config.php.dist ./README.md ./phpcs.xml ./composer.* > /dev/null 2>&1
-7z a -tzip -xr'!.*' ./build/parler-for-wordpress.zip ../parler-for-wordpress
-echo "Release zipped in /build directory, please run \"composer.phar install && git checkout -- .\" to get back dev files"
+composer install --no-ansi --no-dev --no-interaction --no-progress --no-scripts --optimize-autoloader
+
+# Cleanup and build zip
+rm -r build/ .git/ .gitignore .idea/ config.php config.php.dist README.md release.sh phpcs.xml composer.* > /dev/null 2>&1
+7z a -tzip -xr'!.*' ../parler-for-wordpress.zip ./
+echo "Release zipped in /build directory, please run \"composer install && git checkout -- .\" to get back dev files"
