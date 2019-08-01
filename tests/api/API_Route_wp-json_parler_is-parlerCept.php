8117ce693a74524bf3f9af0e396896247c039c09
John Dee <jkarlinski@parler.com>
2019-07-31 18:07:59 -0700

    send to remote

--- tests/api/API_Route_wp-json_parler_is-parlerCept.php
+++ tests/api/API_Route_wp-json_parler_is-parlerCept.php
@@ -1,10 +1,9 @@
 <?php
 
 $I = new ApiTester($scenario);
+
 $I->wantTo('See the WordPress plugin is activated by hitting a custom API');
+$I->expect("the API to return 'true'");
 
-$I->haveHttpHeader('Content-Type', 'application/x-www-form-urlencoded');
-$I->sendGET('/wp-json/parler/is-parler', []);
-
-$I->seeResponseCodeIs(\Codeception\Util\HttpCode::OK); // html code 200 is expected
+$I->sendGET('/wp-json/parler/is-parler');
 $I->seeResponseContains('true');
