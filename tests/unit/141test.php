e946e9794e55cc410b67f2e4726833fd13b20dcc
John Dee <jkarlinski@parler.com>
2019-08-05 20:43:07 -0700

    db user name2

--- tests/unit/141test.php
+++ tests/unit/141test.php
@@ -9,8 +9,8 @@ class CityObjectTest extends \Codeception\TestCase\WPTestCase{
      * it should be instantiatable
      */
     public function it_should_be_instantiatable(){
-        //$stubAnchorLink = $this->stubAnchorLink;
-       // $CityObject = new CRGDaily\CityObject($stubAnchorLink);
+        $stubAnchorLink = $this->stubAnchorLink;
+         $CityObject = new CRGDaily\CityObject($stubAnchorLink);
     }
     
 }
