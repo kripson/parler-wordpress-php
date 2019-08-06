dabf4bc83a8197ebb43e08f4eacfb433cc6e4f51
John Dee <jkarlinski@parler.com>
2019-08-05 20:48:38 -0700

    rename3

---
+++ tests/unit/FirstTest.php
@@ -0,0 +1,15 @@
+<?php
+
+//wtf
+
+class CityObjectTest extends \Codeception\TestCase\WPTestCase{
+    /**
+     * @test
+     * it should be instantiatable
+     */
+    public function it_should_be_instantiatable(){
+        $stubAnchorLink = $this->stubAnchorLink;
+        $CityObject = new CRGDaily\CityObject($stubAnchorLink);
+    }
+
+}
